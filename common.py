e8e6b3d6779b0e6963b083a6fe39a173cf13ffa7
thiagojobson <thiagojobson@gmail.com>
2015-11-07 20:46:09 -0300

    added a commentary in common

--- common.py
+++ common.py
@@ -1,7 +1,11 @@
 GAME_WIDTH = 416
 GAME_HEIGHT = 640
 MAZE_WIDTH = 23
+
+# be careful when changing this value
+# may cause walls to look graphically incomplete
 MAZE_HEIGHT = 50
+
 TILE_SIZE = 32
 ENABLE_GRAPHICS = True
 ENABLE_SOUND = True
